5e941dc04b93cbd139061462ecc6c139f7154c13
Akshitasharma1107 <akshshiv111@gmail.com>
2026-04-30 13:17:46 +0530

    1st commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+
+from flask import Flask, render_template, jsonify,request
 from flask_cors import CORS
 
 # IMPORT CORE LOGIC
@@ -122,7 +123,9 @@ def best_match_full():
         }
     })
 
-
+@app.route("/")
+def home():
+    return render_template("index.html")
 # ---------------- RUN SERVER ----------------
 if __name__ == "__main__":
     app.run(debug=True)
